ec4d3af3c750f5db57accde05db1a347d1e18e51
SuNShine864 <bhavyawadhwa79@gmail.com>
2026-05-04 20:34:35 +0530

    backend setup

--- backend/src/controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-
-const registerUser=asyncHandler((req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
-})
-
-export {registerUser}
